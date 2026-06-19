abb663989d005c8b7333af356d8e1dacb7cf7682
Dmytro <dimus20@gmail.com>
2026-06-19 15:44:34 +0300

    HomeWork Lesson 5.1

--- HW_Lesson5_1.py
+++ HW_Lesson5_1.py
@@ -3,18 +3,22 @@ import keyword
 
 my_string = input("Введіть рядок: ")
 
-punctuation = string.punctuation.replace("_", "__")
+punctuation = string.punctuation.replace("_", "")
 punctuation +=" "
 
 myres=True
+
 if my_string == "":
     myres=False
 
-myres = (not my_string[0].isdigit()) & (my_string not in keyword.kwlist)
+if myres:
+    if my_string[0].isdigit() or my_string in keyword.kwlist or "__" in my_string:
+        myres = False
 
 if myres:
     for char in my_string:
-        if char in punctuation or  (char.isspace() or char.isupper()):
-            myres=False
+        if char.isspace() or char.isupper() or char in punctuation:
+             myres = False
 
 print(myres)
+
