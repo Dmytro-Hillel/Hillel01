a13fc08b84e43af5bd365bde4ed91d301c2ae9ea
Dmytro <dimus20@gmail.com>
2026-06-23 00:08:53 +0300

    HomeWork Lesson 8.3

--- HW_Lesson8_3.py
+++ HW_Lesson8_3.py
@@ -4,8 +4,7 @@ def find_unique_value(mylist:list):
             return item
     return None
 
+
 print([1, 2, 1, 1],"=>",find_unique_value([1, 2, 1, 1]))
 print([2, 3, 3, 3, 5, 5],"=>",find_unique_value([2, 3, 3, 3, 5, 5]))
 print([5, 5, 5, 2, 2, 0.5],"=>",find_unique_value([5, 5, 5, 2, 2, 0.5]))
-
-
