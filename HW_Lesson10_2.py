b6b09711c405c7abb2e9b1043c2a6adfc1c611e8
Dima <dimus20@gmail.com>
2026-07-04 21:50:50 +0300

    HomeWork Lesson 10.2

--- HW_Lesson10_2.py
+++ HW_Lesson10_2.py
@@ -1,23 +1,23 @@
 import string
 
-def replace_punkts(text):
-    punctuation = string.punctuation.replace("'", "")
 
-    symb_repl = set(punctuation) & set(text)
+def replace_punkts(text: str) -> str:
+    symb_repl = set(string.punctuation.replace("'", "")) & set(text)
 
     for symb in symb_repl:
         text = text.replace(symb, " ")
     return (text)
 
 
-def first_word(mystring:str)->str:
-    # mystring = mystring.replace(' ', '')
-    # punctuation = string.punctuation
-    # punctuation = punctuation.replace("'", "")
-    # print(punctuation)
-    return list(map(replace_punkts,mystring))
+def first_word(mystring: str)->str:
+    my_temp_str = replace_punkts(mystring)
+
+    return list(my_temp_str.split())[0]
 
 
-
-print(replace_punkts("просто текст! Еще текст."))
-
+print(first_word("Hello world"))
+print(first_word("greetings, friends"))
+print(first_word("don't touch it"))
+print(first_word(".., and so on ..."))
+print(first_word("hi"))
+print(first_word("Hello.World"))
