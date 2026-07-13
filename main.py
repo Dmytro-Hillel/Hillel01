f554f24fe1c0ba52f54267214e2ea726985a0073
Dima <dimus20@gmail.com>
2026-07-14 00:42:56 +0300

    HomeWork Lesson 15.1

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-print("Hello World!")
+from class_Student import Student
+from class_Group import Group
+from class_MyException import MyException
+
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+
+gr = Group('PD1')
+
+gr.add_student(st1)
+gr.add_student(st2)
+print(gr)
+
+mytestname=['jobs', 'Jobs2']
+
+for i in mytestname:
+    if gr.find_student(i) == st1:
+        print("Знайдено студента " + i)
+    else:
+        print("Не знайдено студента " + i)
+
+gr.delete_student('Taylor')
+print(gr)
+
 
